dbb92e0f98caaae5ec27e4ca1334805ccb9e4330
crutchy <jared.crutchfield@wideband.net.au>
2019-09-05 23:00:20 +1000

    fixed common settings

--- doc/webdb_common_settings.php
+++ doc/webdb_common_settings.php
@@ -3,5 +3,8 @@
 $settings["apps_list"]=array("my_app");
 
 $settings["db_pwd_path"]="/home/user/dev/pwd/";
+$settings["db_admin_file"]=$settings["db_pwd_path"]."sql_admin";
+$settings["db_user_file"]=$settings["db_pwd_path"]."sql_user";
+
 $settings["pdf_temp_path"]="/home/user/dev/temp/";
 $settings["sql_log_path"]="/home/user/dev/log/";
