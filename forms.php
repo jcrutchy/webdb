1c2548f50f14c0a9425af529d4dd5d99e45a61b8
crutchy <jared.crutchfield@wideband.net.au>
2019-10-24 16:12:44 +1100

    lookup post param field process

--- forms.php
+++ forms.php
@@ -1741,8 +1741,6 @@ function process_form_data_fields($form_name,$post_override=false)
     {
       continue;
     }
-    var_dump($_POST);
-    die;
     switch ($control_type)
     {
       case "text":
